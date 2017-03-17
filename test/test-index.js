6f3be8b027c170761f7a0d39bcad1782f1cf71a4
chasingSublimity <blakemsager@gmail.com>
2017-03-17 14:59:24 -0500

    add tests

---
+++ test/test-index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import chai from 'chai';
+
+const should = chai.should();
+
+import App from '../js/components/app';
+
+describe('App component', function() {
+	it('renders the app',  function() {
+
+		const renderer = TestUtils.createRenderer();
+		renderer.render(<App />);
+		const result = renderer.getRenderOutput();
+		console.log(result);
+		result.props.className.should.equal('app');
+		result.props.children.should.equal('Hello Big World');
+	});
+});
