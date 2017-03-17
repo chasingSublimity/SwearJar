80bb24eec972fcede3378f8c44b5b7b587d76049
chasingSublimity <blakemsager@gmail.com>
2017-03-17 14:21:59 -0500

    init setup and hello world

---
+++ js/reducers/index.js
@@ -0,0 +1,11 @@
+import * as actions from '../actions/index';
+
+const initialState = {
+	selectedMember: null,
+	mostCommonWord: null,
+	swearCounter: null,
+};
+
+export const reducer = (state=initialState, action) => {
+	let newState = {};
+};
