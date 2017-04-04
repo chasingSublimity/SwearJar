8d7862551bc81bd23089eefaa132047fa034eeb6
chasingSublimity <blakemsager@gmail.com>
2017-04-04 00:54:49 -0500

    additional styling and attempt to implement loading spinner

--- js/store.js
+++ js/store.js
@@ -1,10 +1,18 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
+// import {pendingTasksReducer} from 'react-redux-spinner'; // The redux reducer
 
 import {reducer} from './reducers/index';
 
+// const combinedReducer = combinedReducer({
+// 	// used for the react spinner
+// 	pendingTasks: pendingTasksReducer,
+// 	// used for the rest of the state
+// 	reducer
+// });
 
 const store = createStore(
+	// combinedReducer,
 	reducer,
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 	applyMiddleware(thunk));
