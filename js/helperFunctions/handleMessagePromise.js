abdb441e9fea00ee55ecf2fbcd9c576dfcf1b1a2
chasingSublimity <blakemsager@gmail.com>
2017-04-07 10:22:14 -0500

    refactor helperFunction to handle edge case where message array < 100

--- js/helperFunctions/handleMessagePromise.js
+++ js/helperFunctions/handleMessagePromise.js
@@ -5,7 +5,10 @@ export default function getMessages(groupId, apiKey, before_id='', messageArray=
 		const url = `https://api.groupme.com/v3/groups/${groupId}/messages?limit=100&token=${apiKey}&before_id=${before_id}`;
 		axios.get(url).then(apiResponse => {
 			const messages = apiResponse.data.response.messages;
-			if (apiCallCounter === 10) {
+			if (messages.length < 100) {
+				messageArray.push(...messages);
+				resolve(messageArray);
+			} else if (apiCallCounter === 10) {
 				resolve(messageArray);
 			} else {
 				messageArray.push(...messages);
